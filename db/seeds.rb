ad14c70c20289aad6d5ebe12655cfdc76d7d2e56
savka-olia <tokarchykolia@gmail.com>
2022-07-17 20:44:30 +0300

    appointments model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.first_or_create(mobile_number:"0968762861", password:"password123")
+
+patient1 = user.patients.find_or_create_by(name:"Ivan Ivanuik")
+patient2 = user.patients.find_or_create_by(name:"Maria Martynuik")
+
+doctor1 = Doctor.find_or_create_by(name:"Iryna Kuts", specializations:"Cardiologists")
+doctor2 = Doctor.find_or_create_by(name:"Natalia Mechnykova", specializations:"Dermatologists")
